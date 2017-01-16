d72d092bc3050b862ea4b5299e8232a1da78a845
Altynbek Karjaubayev <anuar@macbook-pro-anuar.local>
2017-01-16 13:55:56 +0500

    Initial Commit

---
+++ Walkthrough Screen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Walkthrough Screen
+//
+//  Created by Altynbek Karjaubayev on 1/16/17.
+//  Copyright © 2017 Lementum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
