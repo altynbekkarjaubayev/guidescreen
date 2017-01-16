62762580de341435b6d27ae79d47b0a799819186
Altynbek Karjaubayev <anuar@macbook-pro-anuar.local>
2017-01-16 15:31:13 +0500

    default Collection View setup with Anchoring

---
+++ Walkthrough Screen/Helpers.swift
@@ -0,0 +1,42 @@
+//
+//  Helpers.swift
+//  Walkthrough Screen
+//
+//  Created by Altynbek Karjaubayev on 1/16/17.
+//  Copyright © 2017 Lementum. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    // MARK: - Anchor
+    
+    /* if we do not create this extension we have to write this line of code below for each anchor
+     collectionView.topAnchor.constraintEqualToAnchor(view.topAnchor).active = true */
+    
+    func anchorTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
+        anchorWithConstantsToTop(top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+    }
+    
+    func anchorWithConstantsToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat =  0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraintEqualToAnchor(top, constant: topConstant).active = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraintEqualToAnchor(bottom, constant: bottomConstant).active = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraintEqualToAnchor(left, constant: leftConstant).active = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraintEqualToAnchor(right, constant: rightConstant).active = true
+        }
+    }
+}
--- Walkthrough Screen/ViewController.swift
+++ Walkthrough Screen/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.redColor()
+        cv.dataSource = self
+        cv.delegate = self
+        return cv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(collectionView)
+        collectionView.anchorTop(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+        
+        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
-
-
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath)
+        cell.backgroundColor = .blackColor()
+        
+        return cell
+    }
 }
 
